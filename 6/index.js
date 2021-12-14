b7743e16b0f9ac985b71e69f05398a23865ea8ee
JamieAbbottScott <j.scott@crownoil.co.uk>
2021-12-14 08:35:01 +0000

    Day 6

---
+++ 6/index.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const { readFileSync } = require("fs");
+
+const rows = readFileSync(path.join(__dirname, "input.txt"), {
+  encoding: "utf-8",
+}).split(",");
+
+const fish = new Array(7).fill(0);
+const totalFish = rows.length;
+
+for (let i = 0; i < totalFish; i++) {
+  fish[rows[i]]++;
+}
+
+part1(fish.slice(0), totalFish);
+part2(fish.slice(0), totalFish);
+
+function part1(fish, total) {
+  total += simulateFish(fish, 80);
+
+  console.log(`Part 1: ${total}`);
+}
+
+function part2(fish, total) {
+  total += simulateFish(fish, 256);
+
+  console.log(`Part 2: ${total}`);
+}
+
+function simulateFish(fish, days) {
+  const babyFish = new Array(days + 8).fill(0);
+  let total = 0;
+
+  for (let i = 0; i < days; i++) {
+    const fishIndex = i % 7;
+
+    fish[fishIndex] += babyFish[i];
+    babyFish[i + 9] = fish[fishIndex];
+
+    total += fish[fishIndex];
+  }
+
+  return total;
+}

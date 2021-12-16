c90da10870dfc56ed9fcb4c76f4128ba49660111
JamieAbbottScott <j.scott@crownoil.co.uk>
2021-12-16 08:37:55 +0000

    Day 10

---
+++ 10/index.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const { readFileSync } = require("fs");
+const { EOL } = require("os");
+
+const rows = readFileSync(path.join(__dirname, "input.txt"), {
+  encoding: "utf-8",
+})
+  .split(EOL)
+  .map((v) => v.split(""));
+
+solve(rows);
+
+function solve(rows) {
+  const syntaxValueMap = {
+    "(": -3,
+    ")": 3,
+    "[": -57,
+    "]": 57,
+    "{": -1197,
+    "}": 1197,
+    "<": -25137,
+    ">": 25137,
+  };
+
+  const autocompleteMap = {
+    [-3]: 1,
+    [-57]: 2,
+    [-1197]: 3,
+    [-25137]: 4,
+  };
+
+  const totalRows = rows.length;
+  const autocompleteValues = [];
+  let syntaxErrorSum = 0;
+
+  for (let i = 0; i < totalRows; i++) {
+    const totalCols = rows[i].length;
+    const syntaxValues = [syntaxValueMap[rows[i][0]]];
+    let syntaxError = false;
+
+    for (let j = 1; j < totalCols; j++) {
+      const curSyntaxValue = syntaxValueMap[rows[i][j]];
+
+      if (curSyntaxValue < 0) {
+        syntaxValues.push(curSyntaxValue);
+      } else {
+        const lastSyntaxValue = syntaxValues.pop();
+
+        if (lastSyntaxValue * curSyntaxValue !== -(curSyntaxValue ** 2)) {
+          syntaxErrorSum += curSyntaxValue;
+          syntaxError = true;
+          break;
+        }
+      }
+    }
+
+    if (!syntaxError) {
+      let autocompleteValue = 0;
+
+      for (let j = syntaxValues.length; j-- > 0; ) {
+        autocompleteValue =
+          autocompleteValue * 5 + autocompleteMap[syntaxValues[j]];
+      }
+
+      autocompleteValues.push(autocompleteValue);
+    }
+  }
+
+  autocompleteValues.sort((a, b) => a - b);
+
+  const autocompleteValue =
+    autocompleteValues[(autocompleteValues.length - 1) / 2];
+
+  console.log(`Part 1: ${syntaxErrorSum}`);
+
+  console.log(`Part 2: ${autocompleteValue}`);
+}
